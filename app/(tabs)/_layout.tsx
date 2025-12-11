8c26f4d31ea0a1ca06a1579e26d3b88b9b506b3b
malena <malena.guallar@gmail.com>
2025-12-11 16:17:43 +0100

    New layout with home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Tabs } from "expo-router";
+import React from "react";
 
 const TabLayout = () => {
   return (
@@ -10,9 +10,16 @@ const TabLayout = () => {
         tabBarInactiveTintColor: "#6b7280",
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "Camera" }}></Tabs.Screen>
-      <Tabs.Screen name="editor" options={{ title: "Editor" }}></Tabs.Screen>
+      <Tabs.Screen name="index" options={{
+        title: "Home",
+        headerShown: false
+      }}/>
+      <Tabs.Screen name="camera" options={{ title: "Camera" }}></Tabs.Screen>
       <Tabs.Screen name="gallery" options={{ title: "Gallery" }}></Tabs.Screen>
+      <Tabs.Screen name="editor" options={{ 
+        href: null,
+        headerShown: false
+       }}></Tabs.Screen>
     </Tabs>
   );
 };
