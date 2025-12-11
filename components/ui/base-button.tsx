fa48c8abd6526da18267c7c767cd5227d7346248
malena <malena.guallar@gmail.com>
2025-12-11 16:18:44 +0100

    UI : Base Button

---
+++ components/ui/base-button.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+interface BaseButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+const BaseButton = ({ title, onPress }: BaseButtonProps) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 10,
+    backgroundColor: "transparent",
+    width: "60%",
+    borderColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff"
+  }
+});
+
+export default BaseButton;
